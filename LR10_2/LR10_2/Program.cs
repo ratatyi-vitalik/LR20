d6d5959a53e516efe8a5cce167a0c83c53ffb822
ratatyi-vitalik <stasmikolaichik@gmail.com>
2025-11-19 21:12:19 +0300

    First commit

---
+++ LR10_2/LR10_2/Program.cs
@@ -0,0 +1,37 @@
+﻿int Gen()
+{
+    Random rand = new Random();
+    int res = rand.Next(32768, 100000);
+    return res;
+}
+
+int num = Gen();
+Console.WriteLine(num);
+try
+{
+    try
+    {
+        checked
+        {
+            short num1 = (short)num;
+            Console.WriteLine("Результат работы второго блока try: " + num1);
+        }
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine("Сработал внутренний обработчик");
+        throw;
+    }
+}
+catch
+{
+    Console.WriteLine("Сработал внешний обработчик");
+}
+finally
+{
+    unchecked
+    {
+        short fin = (short)Gen();
+        Console.WriteLine("Результат работы блока finaly: " + fin);
+    }
+}
